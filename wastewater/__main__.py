5a8222fcc67a95ca98fe0a32694ea30231b94028
Harris Lapiroff <harris@freedom.press>
2022-10-13 14:55:33 -0400

    Revert to old URL format as page seems to have changed back

--- wastewater/__main__.py
+++ wastewater/__main__.py
@@ -4,13 +4,16 @@ import requests
 import tabula
 from bs4 import BeautifulSoup
 
+LINK_PREFIX = 'MWRAData'
+LINK_SUFFIX = '-data.pdf'
+
 def main():
     res = requests.get('https://www.mwra.com/biobot/biobotdata.htm')
     base_url = "/".join(res.url.split("/")[:-1]) + '/'
 
     soup = BeautifulSoup(res.text, 'html.parser')
-    link = soup.select('a[href^="2022/MWRAData"][href$="-data.pdf"]')[0].attrs['href']
-    date_string = link[13:-9]
+    link = soup.select(f'a[href^="{LINK_PREFIX}"][href$="{LINK_SUFFIX}"]')[0].attrs['href']
+    date_string = link[len(LINK_PREFIX):-len(LINK_SUFFIX)]
     pdf_url = base_url + link
 
     df = tabula.io.convert_into(pdf_url, pages='all', lattice=True, output_path=f'output/mwra-data-{date_string}.csv', output_format='csv')
